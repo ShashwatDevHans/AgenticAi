ec86f81bab2e83b9ccc636ee96eecc774b6658f6
ShashwatDevHans <shashwatdevchaudhry@rediffmail.com>
2025-11-01 16:59:55 +0530

    myfirstcommit

---
+++ LocalShashwatChaudhry.bat
@@ -0,0 +1,4 @@
+@echo off
+git config user.email "shashwatdevchaudhry@rediffmail.com"
+git config user.name "ShashwatDevHans"
+pause
---
+++ git-init-add-status.bat
@@ -0,0 +1,5 @@
+@echo off
+git init
+git add -A
+git status
+PAUSE
